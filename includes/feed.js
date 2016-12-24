63290c01b7eaa4dc6dde0483ba5f3100fb6983fd
Brian Bancroft <hello@brianbancroft.ca>
2016-12-24 15:07:24 -0500

    Reorganizing

--- includes/feed.js
+++ includes/feed.js
@@ -11,15 +11,3 @@ var subsequentBusFeed = function (routeNum, time) {
     return `http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=ttc&r=${route}&t=${time}`
 }
 
-// General feed
-
-function httpGetAsync(theUrl, callback)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
-}
