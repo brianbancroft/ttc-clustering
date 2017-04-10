fd24877753581b68c7a022826b6e082745c8d910
Brian Bancroft <hello@brianbancroft.ca>
2017-04-09 22:28:31 -0400

    🚧  BARE TEMPLATE FOR REQUESTS

--- app.js
+++ app.js
@@ -6,6 +6,33 @@ const https = require('https')
  
 app.set('view engine', 'ejs')
 
+function performRequest(endpoint, success, lastTime = 'na') {
+  const bareEndPoint = 'http://webservices.nextbus.com/service/publicXMLFeed?command=vehicleLocations&a=ttc&r=60'
+
+  if (lastTime !== 'na') {
+    let tempEndPoint = `${bareEndPoint}`
+  } else {
+    let tempEndPoint = `${bareEndPoint}/t=${lastTime}`
+  }
+
+  const req = https.request(options, (res) => {
+    res.setEncoding('utf-8')
+    let responseString = ''
+
+    res.on('data', (data) => {
+      responseString += data
+    })
+
+    res.on('end', () => {
+      console.log('responseString')
+      let responseObject = JSON.parse(responseString)
+      success(responseObject)
+    })
+  })
+  req.write(dataString)
+  req.end()
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
