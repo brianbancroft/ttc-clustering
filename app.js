35987c1a5d05a92f8aefa89b78be6bc1e676e2a1
Brian Bancroft <hello@brianbancroft.ca>
2017-05-20 18:35:31 -0400

    🚧  🔥  Setup of modules, removal of code

--- app.js
+++ app.js
@@ -71,42 +71,25 @@ HomePageController.homePage = (req, res) => {
   })
 }
 
-HomePageController.turfTest = (req, res) => {
-  // app.get('/turftest', (req, res) => {
-//   const point1 = turf.point([-73.123, 40.1234])
-
-//   console.log(point1)
-// })
-  res.render('index', {})
-}
-
 const BusRecordController = {}
-/*
-{ id: '8570',
-  lon: '-79.600464',
-  routeTag: '60',
-  predictable: 'true',
-  dirTag: '60_0_60D',
-  heading: '73',
-  lat: '43.758518',
-  secsSinceReport: '18' }
-*/
+
 BusRecordController.ingestBusData = (req, res, next) => {
   NextVehicleArrivalSystem.request()
     .then((data) => {
-      const d = new Date()
-      console.log(d.toISOString())
+      // const time = new Date().toISOString()
+
       data.vehicle.map((bus) => {
         // TODO 1) Get time, 2) Get Clustered
         console.log(bus)
-        BusLocation.create({
-          route: Number(bus.routeTag),
-          // time: d.toISOString(), 
-          is_clustered: false,
-          direction_tag: bus.dirTag,
-          heading: Number(bus.heading),
-          point: { type: 'Point', coordinates: [Number(bus.lat),Number(bus.lon)]}
-        })
+        console.log(data.vehicle)
+        // BusLocation.create({
+        //   route: Number(bus.routeTag),
+        //   // time: time, 
+        //   is_clustered: false,
+        //   direction_tag: bus.dirTag,
+        //   heading: Number(bus.heading),
+        //   point: { type: 'Point', coordinates: [Number(bus.lat),Number(bus.lon)]}
+        // })
       })
     })
     .then(() => {
@@ -117,64 +100,6 @@ BusRecordController.ingestBusData = (req, res, next) => {
       res.end('Warning: Data failed to ingest. Situations', {err})
     })
 }
-
-// BusRecordController.showSampleBusData = (req, res, next) => {
-// ORIGINAL FUNCTION: 
-// app.get('/sample', (req, res) => {
-//   dbMethods.readRecordsOnDateOnRoute({
-//     route: '60',
-//     month: '4',
-//     day: '15'
-//   }, (results) => {
-//     res.write(JSON.stringify({foo: 'bar'}))
-//   })
-// })
-
-
-// *----- SAMPLE READ ALL RECORDS CALL -----*
-// module.exports.readRecordsOnDateOnRoute = (params, callback) => {
-//   let results = {
-//     type: 'FeatureCollection',
-//     features: []
-//   }
-
-//   const query = client.query(`SELECT (id, route, direction_tag, heading, time, is_clustered, ST_AsText(location)) 
-//     FROM cluster_points
-//     WHERE route='${params.route}'
-//     AND extract(month from time) = '${params.month}'
-//     AND extract(day from time) = '${params.day}'
-//   ;`)
-//   query.on('row', (row) => {
-//     const resultArr = ((row['row'].substring(1, (row['row'].length - 1))).split(','))
-
-//     const resultCoords = resultArr[6].substring(7, resultArr[6].length - 2).split(' ')
-          
-//     resultElement = {
-//       type: 'Feature', 
-//       properties: {
-//         id: Number(resultArr[0]),
-//         route: Number(resultArr[1]),
-//         directionTag: resultArr[2],
-//         heading: Number(resultArr[3]),
-//         dateTime: resultArr[4],
-//         isClustered: resultArr[5] === 't' ? true : false
-//       },
-//       geometry: {
-//         type: 'Point',
-//         coordinates: [
-//           Number(resultCoords[0]),
-//           Number(resultCoords[1])
-//         ]
-//       }
-//     }
-//     results.features.push(resultElement)
-
-//   })
-//   query.on('end', () => {
-//     callback(results)
-//   })
-// }
-// * ----- END SAMPLE CALL ----- * 
 
 // ====== MODULES =====
 
@@ -188,74 +113,32 @@ NextVehicleArrivalSystem.request = () => rp({
   json: true
 })
 
-NextVehicleArrivalSystem.checkForClustering = (records) => {
-  // TODO: Using turf.js, determine whether busses are within 75m
+const TurfSetup = {}
+
+TurfSetup.setupBackgroundData = (data) => {
+  return {
+    type: 'FeatureCollection',
+    features: data.map(element => {
+      return {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [Number(data.lon), Number(data.lat)]
+        },
+        properties: {
+          id: Number(data.id)
+        }
+      }
+    })
+  }
 }
 
-// function isBusClose (params) {
-//   const distance = 75 //75 metres
-//   let busIsClose = false
-
-//   dbMethods.readRecordsWithinDistance({
-//     lat: params.lat,
-//     lon: params.lon,
-//     route: params.route,
-//     distance: distance
-//   }, (results) => {
-//     if (results.length > 0) {
-//       busIsClose = true
-//       results.map((result) => {
-//         const row = result.row.substring(
-//           1,
-//           (result.row.length - 1)
-//         ).split(',')
-//         // TODO: Further query Restrictions
-//         if (row[5] === 'f') {
-//           dbMethods.updateClusteredRecord({
-//             id: Number(row[0])
-//           })
-//         }
-//       })
-//     }
-//   })
-
-//   return busIsClose
-// }
-
-// function addNewRecord (output) {
-//   // res.render('bus',{ data: output} )
-//   // Obtains last time
-//   let lastTimeData = output.lastTime.time
-//   const currentTime =  moment(new Date()).format("YYYY-MM-DD HH:mm:ss")
-
-//   output.vehicle.forEach((vehicle) => {
-//     dbMethods.insertRecord({
-//       route: '60',
-//       directionTag: vehicle.dirTag,
-//       heading: vehicle.heading,
-//       time: currentTime,
-//       isClustered: isBusClose({
-//         lon: vehicle.lon,
-//         lat: vehicle.lat,
-//         route: '60'
-//       }),
-//       lon: vehicle.lon,
-//       lat: vehicle.lat,
-//     })
-//   }, this)
-// }
+TurfSetup.getSinglePoint = (bus) => turf.point([Number(bus.lon), Number(bus.lat)])
 
 // ======= ROUTES ================
 
-// Controller dependencies go here
-// const BusController = require('../controllers/busRecordController')
-// const HomePageController = require('..//controllers/homePageController')
-// const { catchErrors } = require('../handlers/errorHandlers')
-
 router.get('/', HomePageController.homePage)
 router.post('/request', BusRecordController.ingestBusData)
-// router.get('/samplequery', BusRecordController.showSampleBusData)
-// router.get('/turftest', HomePageController.testTurfJS)
 
 app.use('/', router)
 
